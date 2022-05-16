fa3796fa0f2d2b009bded228786716a0e41fc5d3
KX _net <kx.net.com@gmail.com>
2022-05-16 14:56:07 +0900

    re:02_02

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -26,6 +26,78 @@ void GameScene::Initialize() {
 
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
+
+	//////拡縮//////
+	//X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = { 1.5,1,0.5 };
+	//スケーリング倍率を行列に設定する
+	Matrix4 matScale =
+		{ worldTransform_.scale_.x,0,0,0,
+		0,worldTransform_.scale_.y,0,0,
+		0,0,worldTransform_.scale_.z,0,
+		0,0,0,1 };
+
+	//単位行列を代入
+	worldTransform_.matWorld_ = 
+		{ 1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1 };
+
+	//////回転//////
+	//X,Y,Z軸周りの回転角を設定
+	worldTransform_.rotation_ = { 0,0,MathUtility::PI / 4 };
+	//Z軸回転行列を宣言
+	Matrix4 matRotZ = 
+		{ cos(worldTransform_.rotation_.z),sin(worldTransform_.rotation_.z),0,0,
+		-sin(worldTransform_.rotation_.z),cos(worldTransform_.rotation_.z),0,0,
+		0,0,1,0,
+		0,0,0,1 };
+	//X軸回転行列を宣言
+	Matrix4 matRotX =
+		{ 1,0,0,0,
+		0,cos(worldTransform_.rotation_.x),sin(worldTransform_.rotation_.x),0,
+		0,-sin(worldTransform_.rotation_.x),cos(worldTransform_.rotation_.x),0,
+		0,0,0,1 };
+	//Y軸回転行列を宣言
+	Matrix4 matRotY =
+		{ cos(worldTransform_.rotation_.y),0,-sin(worldTransform_.rotation_.y),0,
+		0,1,0,0,
+		sin(worldTransform_.rotation_.y),0,cos(worldTransform_.rotation_.y),0,
+		0,0,0,1 };
+
+	//合成用回転行列を宣言し、ZXYの順に合成
+	Matrix4 matRot = matRotZ;
+	matRot *= matRotX;
+	matRot *= matRotY;
+
+	//////平行移動//////
+	//X,Y,Z方向の平行移動を設定
+	worldTransform_.translation_ = { 0,10,0 };
+	//移動量を行列に設定する
+	Matrix4 matTrans = 
+		{ 1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		worldTransform_.translation_.x,worldTransform_.translation_.y,worldTransform_.translation_.z,1 };
+
+
+	//単位行列を代入
+	worldTransform_.matWorld_ =
+	{ 1,0,0,0,
+	0,1,0,0,
+	0,0,1,0,
+	0,0,0,1 };
+	//スケーリング行列を掛ける
+	worldTransform_.matWorld_ *= matScale;
+	//合成用回転行列を掛ける
+	worldTransform_.matWorld_ *= matRot;
+	//平行移動行列を掛ける
+	worldTransform_.matWorld_ *= matTrans;
+
+	//行列の転送
+	worldTransform_.TransferMatrix();
+
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 
